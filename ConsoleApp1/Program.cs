afa629e6b36ac65532a844b3e8c40b31de75c942
Marc Jacobi <marc.jacobi@macaw.nl>
2020-08-04 19:09:37 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+
+            Console.WriteLine(reply.Message);
+        }
+    }
+}
